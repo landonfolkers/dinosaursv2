773a6d5ce47d59d5091ff50bd2b16793f4035734
Michael Schreier <mjschreier1@gmail.com>
2018-01-29 11:24:05 -0700

    component architecture created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { SkillListComponent } from './components/skill-list/skill-list.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfilesComponent,
+    SkillListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
